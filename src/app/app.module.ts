78596d73382cfe7a72b7d4463951312d6bf530b5
Erick Valencia <valenciaf.erick@gmail.com>
2022-07-23 13:10:28 -0500

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsuariosComponent} from "./usuarios/usuarios.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent
   ],
   imports: [
     BrowserModule
