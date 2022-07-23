cd7bd71c29cf0389f13c9c646c612aa6f7dbd1aa
Erick Valencia <valenciaf.erick@gmail.com>
2022-07-23 13:00:01 -0500

    Commit

---
+++ src/app/usuarios/usuarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tabla_usuarios',
+  templateUrl: './usuarios.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class UsuariosComponent {
+  title = 'Prueba';
+}
